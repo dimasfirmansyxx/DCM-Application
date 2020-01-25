c638a22c215249271c2907818516fa3a7f513a4f
dimasfirmansyxx <d9firmansyah@gmail.com>
2020-01-25 22:21:30 +0700

    modify navbar and beranda

--- application/views/beranda/beranda.php
+++ application/views/beranda/beranda.php
@@ -19,7 +19,7 @@
     <div class="container-fluid">
       <div class="row">
         
-        <div class="col-lg-3 col-6">
+        <div class="col-lg-4 col-6">
           <div class="small-box bg-info">
             <div class="inner">
               <h3>150</h3>
@@ -32,7 +32,7 @@
           </div>
         </div>
 
-        <div class="col-lg-3 col-6">
+        <div class="col-lg-4 col-6">
           <div class="small-box bg-success">
             <div class="inner">
               <h3>53</h3>
@@ -45,7 +45,7 @@
           </div>
         </div>
 
-        <div class="col-lg-3 col-6">
+        <div class="col-lg-4 col-6">
           <div class="small-box bg-warning">
             <div class="inner">
               <h3>44</h3>
@@ -54,19 +54,6 @@
             </div>
             <div class="icon">
               <i class="fas fa-tasks"></i>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-6">
-          <div class="small-box bg-danger">
-            <div class="inner">
-              <h3>65<sup style="font-size: 20px">%</sup></h3>
-
-              <p>Siswa melakukan Tes</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-check"></i>
             </div>
           </div>
         </div>
