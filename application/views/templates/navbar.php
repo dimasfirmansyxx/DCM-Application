f6dd0d7e7e016a605256d9626daed44c49cc2fcc
dimasfirmansyxx <d9firmansyah@gmail.com>
2020-02-09 02:17:53 +0700

    modify navbar

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -6,7 +6,7 @@
   <div class="sidebar">
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <div class="image">
-        <img src="<?= base_url() ?>assets/img/user-ava/<?= $this->Clsglobal->user_info($this->session->user_id)["profile_photo"] ?>" class="img-circle elevation-2" alt="User Image">
+        <img src="<?= base_url() ?>assets/img/user-ava/<?= $this->Clsglobal->user_info($this->session->user_id)["profile_photo"] ?>" class="img-circle elevation-2" alt="<?= $this->Clsglobal->user_info($this->session->user_id)["nama"] ?>" style="background-color: #fff">
       </div>
       <div class="info">
         <a href="#" class="d-block"><?= $this->Clsglobal->user_info($this->session->user_id)["nama"] ?></a>
