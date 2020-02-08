60a7b2ec356e11d570b678f1a8637cc8232cda60
dimasfirmansyxx <d9firmansyah@gmail.com>
2020-02-08 19:33:32 +0700

    creating analisis topik paralel

---
+++ application/views/topik/paralel.php
@@ -0,0 +1,62 @@
+<div class="content-wrapper">
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0 text-dark">Analisis Topik : Paralel</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Laporan</a></li>
+            <li class="breadcrumb-item"><a href="#">Analisis Topik</a></li>
+            <li class="breadcrumb-item active">Paralel</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <section class="content">
+    <div class="container-fluid">
+
+      <div class="row mt-3">
+
+        <section class="col-lg-12">
+          <div class="card">
+            <div class="card-header bg-secondary">
+              Analisa
+            </div>
+            <div class="card-body" id="data_area">
+              <p class="text-center">Sedang memuat...</p>
+            </div>
+          </div>
+        </section>
+      </div>
+
+    </div>
+  </section>
+</div>
+
+<script>
+  $(document).ready(function() {
+    var base_url = "<?= base_url() ?>";
+
+    function setButton(attribute,word) {
+      $(attribute).attr("disabled","disabled");
+      $(attribute).html(word);
+    }
+
+    function unsetButton(attribute,word) {
+      $(attribute).removeAttr("disabled");
+      $(attribute).html(word);
+    }
+
+    function load() {
+      $("#data_area").load(base_url + "butirsoal/paralel/show");
+    }
+
+    // load();
+
+  });
+</script>
