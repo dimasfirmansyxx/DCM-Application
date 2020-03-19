257ad158ec30dba5625e87a53973d4d4f0cc8666
dimasfirmansyxx <d9firmansyah@gmail.com>
2020-03-19 13:13:32 +0700

    modify pengelompokkan report

--- application/views/pengelompokan/show.php
+++ application/views/pengelompokan/show.php
@@ -1,14 +1,34 @@
+<table width="100%">
+	<tbody>
+		<tr>
+			<td colspan="17" align="middle"><h4>
+				PENGELOMPOKKAN SISWA PER MASALAH
+			</h4></td>
+		</tr>
+	</tbody>
+</table>
+<table>
+	<tr>
+		<td width="200">Kelas</td>
+		<td width="500">: <?= $this->kelas->get_kelas($id_kelas)['kelas'] ?></td>
+	</tr>
+	<tr>
+		<td width="200">Sekolah</td>
+		<td width="500">: <?= $this->Clsglobal->site_info("nama_sekolah") ?></td>
+	</tr>
+	<tr>
+		<td width="200">Alamat</td>
+		<td width="500">: <?= $this->Clsglobal->site_info("alamat") ?></td>
+	</tr>
+</table>
 <div class="row">
-	<div class="col-12 text-center">
-		<h4>Pengelompokan Siswa per Masalah</h4>
-	</div>
 	<div class="col-12 table-responsive">
 		<table class="table table-bordered" border="1">
 			<thead class="text-center">
 				<tr>
 					<th>No.</th>
 					<th>Masalah</th>
-					<th>Nomor Urut (Kelas)</th>
+					<th>Nomor Urut</th>
 					<th>Jumlah</th>
 				</tr>
 			</thead>
