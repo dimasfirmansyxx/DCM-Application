07172f0748be33ee65f6f93519762f1f29722653
dimasfirmansyxx <d9firmansyah@gmail.com>
2021-01-21 10:52:47 +0700

    membuat pesan pada halaman login

--- application/views/session/login.php
+++ application/views/session/login.php
@@ -1,42 +1,57 @@
-<div class="login-box mt-5">
-  <div class="login-logo">
-    <img src="<?= base_url() ?>assets/img/core/logo.png" height="100">
-  </div>
-  <!-- /.login-logo -->
-  <div class="card">
-    <div class="card-body login-card-body">
-      <div class="alert" role="alert">
-        #alertarea
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-md-7 my-auto">
+      <div class="card">
+        <div class="card-body">
+          <?= $this->Clsglobal->site_info("login_message") ?>
+        </div>
       </div>
-      <p class="login-box-msg">Masuk untuk memulai sesi</p>
-      <form id="frmlogin">
-        <div class="input-group mb-3">
-          <input type="text" name="username" class="form-control" placeholder="Username" required>
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
+    </div>
+    <div class="col-md-1"></div>
+    <div class="col-md-4">
+      <div class="login-box">
+        <div class="login-logo">
+          <img src="<?= base_url() ?>assets/img/core/logo.png" height="100">
+        </div>
+        <!-- /.login-logo -->
+        <div class="card">
+          <div class="card-body login-card-body">
+            <div class="alert" role="alert">
+              #alertarea
             </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" name="password" class="form-control" placeholder="Password" required>
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-12">
-            <button type="submit" class="btn btn-primary btn-block btnlogin">Masuk</button>
-          </div>
-        </div>
-      </form>
+            <p class="login-box-msg">Masuk untuk memulai sesi</p>
+            <form id="frmlogin">
+              <div class="input-group mb-3">
+                <input type="text" name="username" class="form-control" placeholder="Username" required>
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-user"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="input-group mb-3">
+                <input type="password" name="password" class="form-control" placeholder="Password" required>
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-lock"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  <button type="submit" class="btn btn-primary btn-block btnlogin">Masuk</button>
+                </div>
+              </div>
+            </form>
 
-      <p class="mb-0">
-        Belum memiliki akun ?
-        <a href="<?= base_url() ?>auth/register" class="text-center">Registrasi</a>
-      </p>
+            <p class="mb-0">
+              Belum memiliki akun ?
+              <a href="<?= base_url() ?>auth/register" class="text-center">Registrasi</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      
     </div>
   </div>
 </div>
